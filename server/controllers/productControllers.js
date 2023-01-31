ef683bfc707cbb379fe61b0c5caa9512108ce4cc
xuananh95 <xanh.trym95@gmail.com>
2023-01-31 16:01:05 +0700

    fix get product by id route

--- server/controllers/productControllers.js
+++ server/controllers/productControllers.js
@@ -66,6 +66,7 @@ const findProductById = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error("Invalid product id");
     }
+    console.log(id);
     const product = await Product.findById(id);
     if (product) {
         res.status(200).json({
