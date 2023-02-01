3acc7fe46c012c1486556a456c8a5e3c96f591f3
Quy <quy4312@gmail.com>
2023-02-01 15:53:17 +0700

    sua App.js import CaloPage 2 lan

--- client/src/App.js
+++ client/src/App.js
@@ -16,34 +16,33 @@ import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import ProductPage from "./pages/ProductPage/ProductPage";
 import SignInPage from "./pages/SignInPage/SignInPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
-import { CaloPage } from "./pages/CaloPage/CaloPage";
+
 function App() {
-    const { isLogined, isModalSignIn, isModalSignUp } =
-        useContext(StateContext);
-    return (
-        <div className="App">
-            <Header />
-            {isModalSignIn && <SignInPage />}
-            {isModalSignUp && <SignUpPage />}
-            <Routes>
-                <Route path="/" element={<HomePage />} />
+  const { isLogined, isModalSignIn, isModalSignUp } = useContext(StateContext);
+  return (
+    <div className="App">
+      <Header />
+      {isModalSignIn && <SignInPage />}
+      {isModalSignUp && <SignUpPage />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
 
-                <Route path="/calo" element={<CaloPage />} />
-                <Route path="/product" element={<ProductPage />}>
-                    <Route path="*" element={<Card />} />
-                </Route>
+        <Route path="/calo" element={<CaloPage />} />
+        <Route path="/product" element={<ProductPage />}>
+          <Route path="*" element={<Card />} />
+        </Route>
 
-                <Route path="/cart/:id" element={<CartPage />} />
-                <Route path="dasboard" element={<DasboardPage />}>
-                    <Route path="kho-hang" element={<Warehouse />} />
-                    <Route path="them-san-pham" element={<CreateProduct />} />
-                </Route>
+        <Route path="/cart/:id" element={<CartPage />} />
+        <Route path="dasboard" element={<DasboardPage />}>
+          <Route path="kho-hang" element={<Warehouse />} />
+          <Route path="them-san-pham" element={<CreateProduct />} />
+        </Route>
 
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-            <Footer />
-        </div>
-    );
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
